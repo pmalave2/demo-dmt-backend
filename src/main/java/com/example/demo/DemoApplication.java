d80d418c87b511e2146072adba02edbdf8413085
pavemala <pavel.malave@ext.wefox.com>
2023-03-22 13:00:21 +0100

    workflows and code fixes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
 
   public static void main(String[] args) {
+    ApplicationInsights.attach();
     SpringApplication.run(DemoApplication.class, args);
   }
 }
